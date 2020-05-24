be13aff9312eac3b76473e2cd92da41e8e4eb412
DAZUN XING <dazun.xing@gmail.com>
2020-05-24 06:27:30 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+require 'nokogiri'
+require 'open-uri'
+ 
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN")
